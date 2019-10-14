eee1c8a0f8c58601170052f8d0aa805e193c621d
Billy Donahue <billy.donahue@mongodb.com>
2019-10-14 12:11:11 -0400

    prinf

--- asan-sigaltstack-dealloc/asan_test.cpp
+++ asan-sigaltstack-dealloc/asan_test.cpp
@@ -8,6 +8,7 @@
 #include <thread>
 
 void sigAltStackInstall(void* buf, size_t bufSize) {
+    fprintf(stderr, "installing sigaltstack= @%p[%#zx]\n", buf, bufSize);
     stack_t ss;
     ss.ss_sp = buf;
     ss.ss_size = bufSize;
