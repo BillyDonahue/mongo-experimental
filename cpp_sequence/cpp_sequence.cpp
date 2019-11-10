12c958305884b077973571aecdff49ed88a07800
Billy Donahue <billy.donahue@gmail.com>
2019-11-10 17:26:00 -0500

    more experiments

--- cpp_sequence/cpp_sequence.cpp
+++ cpp_sequence/cpp_sequence.cpp
@@ -1,29 +1,42 @@
+#include <iostream>
 
-#define CAT_(a,b) a ## b
-#define CAT(a, b) CAT_(a, b)
+#define NDIGITS 4
 
-#define S0 _
-#define S1 X
+#define PRIMITIVE_CAT(a, b) a ## b
+#define CAT(a, b) PRIMITIVE_CAT(a, b)
 
-// Challenge: Make a tuple of digits that can be applied to a function macro.
-#define D0(a,b,c,d) Dec(a,b,c,d),
-#define D1(a,b,c) \
-    D0(a,b,c,0) D0(a,b,c,1) D0(a,b,c,2) D0(a,b,c,3) D0(a,b,c,4) \
-    D0(a,b,c,5) D0(a,b,c,6) D0(a,b,c,7) D0(a,b,c,8) D0(a,b,c,9)
-#define D2(a,b) \
-    D1(a,b,0) D1(a,b,1) D1(a,b,2) D1(a,b,3) D1(a,b,4) \
-    D1(a,b,5) D1(a,b,6) D1(a,b,7) D1(a,b,8) D1(a,b,9)
-#define D3(a) \
-    D2(a,0) D2(a,1) D2(a,2) D2(a,3) D2(a,4) D2(a,5) \
-    D2(a,6) D2(a,7) D2(a,8) D2(a,9)
-#define D4 D3(0) D3(1) D3(2) D3(3) D3(4) D3(5) D3(6) D3(7) D3(8) D3(9) 
+#define D0(e, ...) e(__VA_ARGS__)
+#define D1(...) \
+    D0(__VA_ARGS__,0) D0(__VA_ARGS__,1) D0(__VA_ARGS__,2) D0(__VA_ARGS__,3) D0(__VA_ARGS__,4) \
+    D0(__VA_ARGS__,5) D0(__VA_ARGS__,6) D0(__VA_ARGS__,7) D0(__VA_ARGS__,8) D0(__VA_ARGS__,9)
+#define D2(...) \
+    D1(__VA_ARGS__,0) D1(__VA_ARGS__,1) D1(__VA_ARGS__,2) D1(__VA_ARGS__,3) D1(__VA_ARGS__,4) \
+    D1(__VA_ARGS__,5) D1(__VA_ARGS__,6) D1(__VA_ARGS__,7) D1(__VA_ARGS__,8) D1(__VA_ARGS__,9)
+#define D3(...) \
+    D2(__VA_ARGS__,0) D2(__VA_ARGS__,1) D2(__VA_ARGS__,2) D2(__VA_ARGS__,3) D2(__VA_ARGS__,4) \
+    D2(__VA_ARGS__,5) D2(__VA_ARGS__,6) D2(__VA_ARGS__,7) D2(__VA_ARGS__,8) D2(__VA_ARGS__,9)
+#define D4(...) \
+    D3(__VA_ARGS__,0) D3(__VA_ARGS__,1) D3(__VA_ARGS__,2) D3(__VA_ARGS__,3) D3(__VA_ARGS__,4) \
+    D3(__VA_ARGS__,5) D3(__VA_ARGS__,6) D3(__VA_ARGS__,7) D3(__VA_ARGS__,8) D3(__VA_ARGS__,9)
+#define D5(...) \
+    D4(__VA_ARGS__,0) D4(__VA_ARGS__,1) D4(__VA_ARGS__,2) D4(__VA_ARGS__,3) D4(__VA_ARGS__,4) \
+    D4(__VA_ARGS__,5) D4(__VA_ARGS__,6) D4(__VA_ARGS__,7) D4(__VA_ARGS__,8) D4(__VA_ARGS__,9)
 
+#define MAKE_OBJ(...) Dec{__VA_ARGS__},
 
-struct Dec { int x; };
-Dec decs[] = {
-  D4
+struct Dec { int digits[NDIGITS]; };
+constexpr Dec decs[] = {
+  CAT(D, NDIGITS) (MAKE_OBJ)
 };
 
 int main() {
-    return 0;
+  for (auto d : decs) {
+    std::cout << "(";
+    for (auto&& dig : d.digits) {
+      std::cout << dig;
+    }
+    std::cout << ")";
+  }
+  std::cout << "\n";
+  return 0;
 }
